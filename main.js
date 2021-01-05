68f07a7b2e338be5bbedc0730fd7c4d05a6bd3d3
yh_pups <2944809997@qq.com>
2021-01-05 18:10:08 +0800

    #fa

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ import App from './App'
 // 此处为演示vuex使用，非uView的功能部分
 import store from '@/store';
 
+import SocketIm from  "./common/pps-uni-im-socket-jdk.js"
+
+SocketIm.initWsUrl("ws://localhost:9090/pps")
+Vue.prototype.$im=SocketIm;
+
 // 引入uView提供的对vuex的简写法文件
 let vuexStore = require('@/store/$u.mixin.js');
 Vue.mixin(vuexStore);
 // 引入uView对小程序分享的mixin封装
 let mpShare = require('uview-ui/libs/mixin/mpShare.js');
 Vue.mixin(mpShare);
-
-
+import Im from 'common/pps-uni-im-socket-jdk.js'
 Vue.config.productionTip = false
 Vue.prototype.log=true;
 App.mpType = 'app'
@@ -27,7 +31,7 @@ const app = new Vue({
 // http拦截器，将此部分放在new Vue()和app.$mount()之间，才能App.vue中正常使用
 import httpInterceptor from '@/common/http.interceptor.js';
 Vue.use(httpInterceptor, app);
-
+Vue.use(Im,app)
 // http接口API抽离，免于写url或者一些固定的参数
 import httpApi from '@/common/http.api.js';
 Vue.use(httpApi, app);
